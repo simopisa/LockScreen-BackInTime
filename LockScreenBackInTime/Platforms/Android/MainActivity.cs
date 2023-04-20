7424d4329efd74f9108b8cf5daf9091159ca22e0
simopisa <48206660+simopisa@users.noreply.github.com>
2023-04-20 15:57:50 +0200

    Adedd files

---
+++ LockScreenBackInTime/Platforms/Android/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace LockScreenBackInTime;
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, Label = "LockScreen",
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
+public class MainActivity : MauiAppCompatActivity
+{
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        Platform.Init(this, savedInstanceState);
+        this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+    }
+}
+
+
